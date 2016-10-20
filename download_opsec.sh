5023a962acaa22d2b0891f6b24a9eee1d4ed092b
Ulrik Holmen <ulrik.holmen@nttcomsecurity.com>
2016-10-20 13:43:28 +0200

    New URL for opsec library

--- download_opsec.sh
+++ download_opsec.sh
@@ -6,8 +6,8 @@
 #
 # Before building: sudo apt-get install gcc-multilib g++-multilib libelf-dev:i386
 
-OPSEC_DOWNLOAD="http://dl3.checkpoint.com/paid/9a/OPSEC_SDK_6.0_Linux.zip?HashKey=1459193380_ef2956a1ffa7eee00ea6468d2a7494ba&xtn=.zip"
-OPSEC_ZIPFILE="OPSEC_SDK_6.0_Linux.zip"
+OPSEC_DOWNLOAD="http://dl3.checkpoint.com/paid/8e/8ee4831512ee5a52fb484bdc364c4d75/Check_Point_OPSECSDK_linux50_sk110425.tar.gz?HashKey=1476970892_3b806b01e0dac8bdf5e577424e664686&xtn=.gz"
+OPSEC_ZIPFILE="Check_Point_OPSECSDK_linux50_sk110425.tar.gz"
 URL=$(wget "http://supportcontent.checkpoint.com/file_download?id=7385" -O - | grep ".zip" | awk -F "href=" '{print $2}' | sort | uniq | sed 's/[>\s\"]//g' | cat -v | sed 's/\^M//g' )
 OPSEC_DIR="OPSEC"
 
